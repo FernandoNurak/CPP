c8fd52b9fdf6bcdd94df689ba2e4f7b082a844e0
FernandoNurak <150642903+FernandoNurak@users.noreply.github.com>
2024-05-21 05:55:15 +0700

    menyelesaikan tugas15
    tugas 15 telah selesai

--- Tugas15.cpp
+++ Tugas15.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void tangkap(){
-
+void tangkap(int *a,int *s){
+    cout << "hasil :"<< *a * *s;
 }
 int main(){
     int x,y;
-    cout << "input nilai x :"<<endl;
-        
+    cout << "input nilai x : ";
+    cin >> x;
+    cout << "input nilai y : ";
+    cin >> y;
     
+    tangkap(&x,&y);
+
+return 0;
 }
